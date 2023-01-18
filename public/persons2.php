332e5459a326f3b0255d90cf7ad36ac1621a6b56
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-01-18 16:02:29 +0000

    CRU person

--- public/persons2.php
+++ public/persons2.php
@@ -2,9 +2,16 @@
 require_once("../src/connection.php");
 
 use Dsw\Blade\Person as Person;
+use Philo\Blade\Blade;
+
+$views = '../views';
+$cache = '../cache';
+
+$blade = new Blade($views, $cache);
 
 $persons = Person::all();
 
-foreach($persons as $person) {
-  echo "<p>" . $person->name . "</p>";
-}
+// foreach($persons as $person) {
+//   echo "<p>" . $person->name . "</p>";
+// }
+echo $blade->view()->make('layouts.persons', compact('persons'))->render();
